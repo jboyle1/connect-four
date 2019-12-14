188423595ed2e669001539623fae62424d850b82
Jed Boyle <jboyle1@pca.ac.uk>
2019-12-14 15:55:04 +0000

    Added functions to check where the bottom is in each column and a function to check the color match.

--- static/js/myscript.js
+++ static/js/myscript.js
@@ -7,12 +7,42 @@ var player2Color = 'rgb(237, 45, 73)';
 var game_on = true;
 var table = $('table tr');
 
-function reportWin(rowNum,colNum) {
+function reportWin(rowNum, colNum) {
     console.log("You wone starting at this row, col");
     console.log(rowNum);
     console.log(colNum);
 }
 
-function changeColor(rowIndex,colIndex,color) {
-    return table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-color',color);
+function changeColor(rowIndex, colIndex, color) {
+    return table
+        .eq(rowIndex)
+        .find('td')
+        .eq(colIndex)
+        .find('button')
+        .css('background-color', color);
+}
+
+function returnColor(rowIndex, colIndex) {
+    return table
+        .eq(rowIndex)
+        .find('td')
+        .eq(colIndex)
+        .find('button')
+        .css('background-color');
+}
+
+function checkBottom(colIndex) {
+    var colorReport = returnColor(5, colIndex);
+    for (var row = 5; row > -1; row--) {
+        colorReport = returnColor(row, colIndex);
+        if (colorReport === 'rgb(128, 128, 128)') {
+            return row
+        }
+    }
+}
+
+function colorMatchCheck(one, two, three, four) {
+    return (
+        one === two && one === three && one === four && one !== 'rgb(128, 128, 128)' && one !== undefined
+    )
 }
