fd45bf10eeebcc79074f814f24a337e6fc91c0f3
Jed Boyle <jboyle1@pca.ac.uk>
2019-12-14 16:07:45 +0000

    Added function to check for horizontal wins.

--- static/js/myscript.js
+++ static/js/myscript.js
@@ -1,4 +1,4 @@
-var player1 = prompt("Player One: Enter Your Name, you will be Blue");
+var player1 = prompt("Player One: Enter Your Name , you will be Blue");
 var player1Color = 'rgb(86, 151, 255)';
 
 var player2 = prompt("Player Two: Enter Your Name, you will be Red");
@@ -7,12 +7,13 @@ var player2Color = 'rgb(237, 45, 73)';
 var game_on = true;
 var table = $('table tr');
 
+// http://stackoverflow.com/questions/6139407/getting-td-by-index-with-jquery
 function reportWin(rowNum, colNum) {
-    console.log("You wone starting at this row, col");
+    console.log("You won starting at this row,col");
     console.log(rowNum);
     console.log(colNum);
 }
-
+// Change the color of a button
 function changeColor(rowIndex, colIndex, color) {
     return table
         .eq(rowIndex)
@@ -22,6 +23,7 @@ function changeColor(rowIndex, colIndex, color) {
         .css('background-color', color);
 }
 
+// Report Back to current color of a button
 function returnColor(rowIndex, colIndex) {
     return table
         .eq(rowIndex)
@@ -31,6 +33,7 @@ function returnColor(rowIndex, colIndex) {
         .css('background-color');
 }
 
+// Take in column index, returns the bottom row that is still gray
 function checkBottom(colIndex) {
     var colorReport = returnColor(5, colIndex);
     for (var row = 5; row > -1; row--) {
@@ -41,8 +44,29 @@ function checkBottom(colIndex) {
     }
 }
 
+// Check to see if 4 inputs are the same color
 function colorMatchCheck(one, two, three, four) {
     return (
         one === two && one === three && one === four && one !== 'rgb(128, 128, 128)' && one !== undefined
-    )
+    );
+}
+
+// Check for Horizontal Wins
+function horizontalWinCheck() {
+    for (var row = 0; row < 6; row++) {
+        for (var col = 0; col < 4; col++) {
+            if (colorMatchCheck(
+                returnColor(row, col),
+                returnColor(row, col + 1),
+                returnColor(row, col + 2),
+                returnColor(row, col + 3)
+            )) {
+                console.log('horiz');
+                reportWin(row, col);
+                return true;
+            } else {
+                continue;
+            }
+        }
+    }
 }
